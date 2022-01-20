9fb89419952c6a32d7b9e7486ee2f7832d68d022
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2022-01-19 18:40:17 -0800

    Detect keys being pressed and fly cowboy

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	_ "image/png"
 	"log"
 )
@@ -10,6 +11,8 @@ import (
 var spaceCowboyImg *ebiten.Image
 var spaceCowboyX float64 = 1
 var spaceCowboyY float64 = 1
+var touchIDs []ebiten.TouchID
+var gamepadIDs []ebiten.GamepadID
 
 func init() {
 	var err error
@@ -27,6 +30,11 @@ func (g *Game) Update() error {
 	if spaceCowboyY > 200 {
 		spaceCowboyY = 200
 	}
+
+	if g.isKeyJustPressed() {
+		spaceCowboyY -= 15
+	}
+
 	return nil
 }
 
@@ -35,6 +43,39 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(1.2, spaceCowboyY )
 	screen.DrawImage(spaceCowboyImg, op)
+}
+
+func (g *Game) isKeyJustPressed() bool {
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		return true
+	}
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		return true
+	}
+	touchIDs = inpututil.AppendJustPressedTouchIDs(touchIDs)
+	if len(touchIDs) > 0 {
+		return true
+	}
+	gamepadIDs = ebiten.AppendGamepadIDs(gamepadIDs[:0])
+	for _, g := range gamepadIDs {
+		if ebiten.IsStandardGamepadLayoutAvailable(g) {
+			if inpututil.IsStandardGamepadButtonJustPressed(g, ebiten.StandardGamepadButtonRightBottom) {
+				return true
+			}
+			if inpututil.IsStandardGamepadButtonJustPressed(g, ebiten.StandardGamepadButtonRightRight) {
+				return true
+			}
+		} else {
+			// The button 0/1 might not be A/B buttons.
+			if inpututil.IsGamepadButtonJustPressed(g, ebiten.GamepadButton0) {
+				return true
+			}
+			if inpututil.IsGamepadButtonJustPressed(g, ebiten.GamepadButton1) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
